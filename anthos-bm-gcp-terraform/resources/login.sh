8087ba954f30ec807f4d60478971f9400e02d7e4
Jordan Liggitt <liggitt@google.com>
2022-05-19 22:31:12 -0400

    Fix method of obtaining service account token

--- anthos-bm-gcp-terraform/resources/login.sh
+++ anthos-bm-gcp-terraform/resources/login.sh
@@ -50,8 +50,24 @@ kubectl create clusterrolebinding another-binding \
   --clusterrole cluster-admin --serviceaccount default:edga-sa
 
 echo "-------------------------------------------------------------------"
-echo "💡 Retreiving Kubernetes Service Account Token"
-SECRET_NAME=$(kubectl get serviceaccount edga-sa -o jsonpath='{$.secrets[0].name}')
+echo "💡 Retrieving Kubernetes Service Account Token"
+SECRET_NAME=edga-sa-token
+
+kubectl apply -f - << __EOF__
+apiVersion: v1
+kind: Secret
+metadata:
+  name: "${SECRET_NAME}"
+  annotations:
+    kubernetes.io/service-account.name: edga-sa
+type: kubernetes.io/service-account-token
+__EOF__
+
+until [[ $(kubectl get -o=jsonpath="{.data.token}" "secret/${SECRET_NAME}") ]]; do 
+  echo "waiting for token..." >&2;
+  sleep 1;
+done
+
 TOKEN=$(kubectl get secret "${SECRET_NAME}" -o jsonpath='{$.data.token}' | base64 --decode)
 
 echo ""
